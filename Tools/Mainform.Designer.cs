3c48bd8fbae8a3e280fb9315e9a7928964a24e1e
LemonGuai <csf2412297817@163.com>
2020-08-15 17:29:04 +0800

    push

--- Tools/Mainform.Designer.cs
+++ Tools/Mainform.Designer.cs
@@ -28,19 +28,360 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+			this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+			this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
+			this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+			this.xtraTabPage1 = new DevExpress.XtraTab.XtraTabPage();
+			this.xtraTabPage2 = new DevExpress.XtraTab.XtraTabPage();
+			this.layoutControl2 = new DevExpress.XtraLayout.LayoutControl();
+			this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+			this.tbMinutes = new System.Windows.Forms.TextBox();
+			this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+			this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+			this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+			this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+			this.btnOption = new DevExpress.XtraEditors.SimpleButton();
+			this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+			this.emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
+			this.emptySpaceItem3 = new DevExpress.XtraLayout.EmptySpaceItem();
+			this.emptySpaceItem4 = new DevExpress.XtraLayout.EmptySpaceItem();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.labelTime = new DevExpress.XtraEditors.LabelControl();
+			this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+			this.emptySpaceItem5 = new DevExpress.XtraLayout.EmptySpaceItem();
+			this.btnReset = new DevExpress.XtraEditors.SimpleButton();
+			this.layoutControlItem6 = new DevExpress.XtraLayout.LayoutControlItem();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+			this.layoutControl1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
+			this.xtraTabControl1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+			this.xtraTabPage1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControl2)).BeginInit();
+			this.layoutControl2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// layoutControl1
+			// 
+			this.layoutControl1.Controls.Add(this.xtraTabControl1);
+			this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+			this.layoutControl1.Name = "layoutControl1";
+			this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(2531, 281, 975, 600);
+			this.layoutControl1.Root = this.Root;
+			this.layoutControl1.Size = new System.Drawing.Size(650, 521);
+			this.layoutControl1.TabIndex = 0;
+			this.layoutControl1.Text = "layoutControl1";
+			// 
+			// Root
+			// 
+			this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+			this.Root.GroupBordersVisible = false;
+			this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+			this.layoutControlItem1});
+			this.Root.Name = "Root";
+			this.Root.Padding = new DevExpress.XtraLayout.Utils.Padding(0, 0, 0, 0);
+			this.Root.Size = new System.Drawing.Size(650, 521);
+			this.Root.TextVisible = false;
+			// 
+			// xtraTabControl1
+			// 
+			this.xtraTabControl1.Location = new System.Drawing.Point(0, 0);
+			this.xtraTabControl1.Name = "xtraTabControl1";
+			this.xtraTabControl1.SelectedTabPage = this.xtraTabPage1;
+			this.xtraTabControl1.Size = new System.Drawing.Size(650, 521);
+			this.xtraTabControl1.TabIndex = 4;
+			this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+			this.xtraTabPage1,
+			this.xtraTabPage2});
+			// 
+			// layoutControlItem1
+			// 
+			this.layoutControlItem1.Control = this.xtraTabControl1;
+			this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+			this.layoutControlItem1.Name = "layoutControlItem1";
+			this.layoutControlItem1.Padding = new DevExpress.XtraLayout.Utils.Padding(0, 0, 0, 0);
+			this.layoutControlItem1.Size = new System.Drawing.Size(650, 521);
+			this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+			this.layoutControlItem1.TextVisible = false;
+			// 
+			// xtraTabPage1
+			// 
+			this.xtraTabPage1.Controls.Add(this.layoutControl2);
+			this.xtraTabPage1.Margin = new System.Windows.Forms.Padding(0);
+			this.xtraTabPage1.Name = "xtraTabPage1";
+			this.xtraTabPage1.Size = new System.Drawing.Size(648, 483);
+			this.xtraTabPage1.Text = "xtraTabPage1";
+			// 
+			// xtraTabPage2
+			// 
+			this.xtraTabPage2.Name = "xtraTabPage2";
+			this.xtraTabPage2.Size = new System.Drawing.Size(1049, 610);
+			this.xtraTabPage2.Text = "xtraTabPage2";
+			// 
+			// layoutControl2
+			// 
+			this.layoutControl2.Controls.Add(this.btnReset);
+			this.layoutControl2.Controls.Add(this.labelTime);
+			this.layoutControl2.Controls.Add(this.btnOption);
+			this.layoutControl2.Controls.Add(this.labelControl1);
+			this.layoutControl2.Controls.Add(this.tbMinutes);
+			this.layoutControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.layoutControl2.Location = new System.Drawing.Point(0, 0);
+			this.layoutControl2.Name = "layoutControl2";
+			this.layoutControl2.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(2865, 458, 975, 404);
+			this.layoutControl2.Root = this.layoutControlGroup1;
+			this.layoutControl2.Size = new System.Drawing.Size(648, 483);
+			this.layoutControl2.TabIndex = 0;
+			this.layoutControl2.Text = "layoutControl2";
+			// 
+			// layoutControlGroup1
+			// 
+			this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+			this.layoutControlGroup1.GroupBordersVisible = false;
+			this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+			this.layoutControlItem2,
+			this.emptySpaceItem1,
+			this.layoutControlItem3,
+			this.emptySpaceItem2,
+			this.layoutControlItem4,
+			this.emptySpaceItem3,
+			this.emptySpaceItem4,
+			this.layoutControlItem5,
+			this.emptySpaceItem5,
+			this.layoutControlItem6});
+			this.layoutControlGroup1.Name = "Root";
+			this.layoutControlGroup1.Padding = new DevExpress.XtraLayout.Utils.Padding(0, 0, 0, 0);
+			this.layoutControlGroup1.Size = new System.Drawing.Size(648, 483);
+			this.layoutControlGroup1.TextVisible = false;
+			// 
+			// tbMinutes
+			// 
+			this.tbMinutes.Location = new System.Drawing.Point(269, 4);
+			this.tbMinutes.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+			this.tbMinutes.Name = "tbMinutes";
+			this.tbMinutes.Size = new System.Drawing.Size(133, 30);
+			this.tbMinutes.TabIndex = 4;
+			this.tbMinutes.Text = "1";
+			this.tbMinutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.tbMinutes.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbMinutes_MouseClick);
+			this.tbMinutes.TextChanged += new System.EventHandler(this.tbMinutes_TextChanged);
+			// 
+			// layoutControlItem2
+			// 
+			this.layoutControlItem2.Control = this.tbMinutes;
+			this.layoutControlItem2.Location = new System.Drawing.Point(213, 0);
+			this.layoutControlItem2.MaxSize = new System.Drawing.Size(191, 36);
+			this.layoutControlItem2.MinSize = new System.Drawing.Size(191, 36);
+			this.layoutControlItem2.Name = "layoutControlItem2";
+			this.layoutControlItem2.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 2, 4, 2);
+			this.layoutControlItem2.Size = new System.Drawing.Size(191, 36);
+			this.layoutControlItem2.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+			this.layoutControlItem2.Text = "定时:";
+			this.layoutControlItem2.TextSize = new System.Drawing.Size(42, 22);
+			// 
+			// emptySpaceItem1
+			// 
+			this.emptySpaceItem1.AllowHotTrack = false;
+			this.emptySpaceItem1.Location = new System.Drawing.Point(0, 278);
+			this.emptySpaceItem1.Name = "emptySpaceItem1";
+			this.emptySpaceItem1.Size = new System.Drawing.Size(648, 205);
+			this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+			// 
+			// labelControl1
+			// 
+			this.labelControl1.Location = new System.Drawing.Point(404, 6);
+			this.labelControl1.Margin = new System.Windows.Forms.Padding(0);
+			this.labelControl1.Name = "labelControl1";
+			this.labelControl1.Size = new System.Drawing.Size(36, 22);
+			this.labelControl1.StyleController = this.layoutControl2;
+			this.labelControl1.TabIndex = 5;
+			this.labelControl1.Text = "分钟";
+			// 
+			// layoutControlItem3
+			// 
+			this.layoutControlItem3.Control = this.labelControl1;
+			this.layoutControlItem3.Location = new System.Drawing.Point(404, 0);
+			this.layoutControlItem3.Name = "layoutControlItem3";
+			this.layoutControlItem3.Padding = new DevExpress.XtraLayout.Utils.Padding(0, 0, 6, 0);
+			this.layoutControlItem3.Size = new System.Drawing.Size(36, 36);
+			this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+			this.layoutControlItem3.TextVisible = false;
+			// 
+			// btnOption
+			// 
+			this.btnOption.Location = new System.Drawing.Point(2, 208);
+			this.btnOption.Margin = new System.Windows.Forms.Padding(0);
+			this.btnOption.Name = "btnOption";
+			this.btnOption.Size = new System.Drawing.Size(644, 32);
+			this.btnOption.StyleController = this.layoutControl2;
+			this.btnOption.TabIndex = 6;
+			this.btnOption.Text = "开始";
+			this.btnOption.Click += new System.EventHandler(this.btnOption_Click);
+			// 
+			// layoutControlItem4
+			// 
+			this.layoutControlItem4.Control = this.btnOption;
+			this.layoutControlItem4.Location = new System.Drawing.Point(0, 206);
+			this.layoutControlItem4.Name = "layoutControlItem4";
+			this.layoutControlItem4.Size = new System.Drawing.Size(648, 36);
+			this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+			this.layoutControlItem4.TextVisible = false;
+			// 
+			// emptySpaceItem2
+			// 
+			this.emptySpaceItem2.AllowHotTrack = false;
+			this.emptySpaceItem2.Location = new System.Drawing.Point(440, 0);
+			this.emptySpaceItem2.Name = "emptySpaceItem2";
+			this.emptySpaceItem2.Size = new System.Drawing.Size(208, 36);
+			this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
+			// 
+			// emptySpaceItem3
+			// 
+			this.emptySpaceItem3.AllowHotTrack = false;
+			this.emptySpaceItem3.Location = new System.Drawing.Point(0, 0);
+			this.emptySpaceItem3.Name = "emptySpaceItem3";
+			this.emptySpaceItem3.Size = new System.Drawing.Size(213, 36);
+			this.emptySpaceItem3.TextSize = new System.Drawing.Size(0, 0);
+			// 
+			// emptySpaceItem4
+			// 
+			this.emptySpaceItem4.AllowHotTrack = false;
+			this.emptySpaceItem4.Location = new System.Drawing.Point(0, 36);
+			this.emptySpaceItem4.Name = "emptySpaceItem4";
+			this.emptySpaceItem4.Size = new System.Drawing.Size(648, 52);
+			this.emptySpaceItem4.TextSize = new System.Drawing.Size(0, 0);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// labelTime
+			// 
+			this.labelTime.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+			this.labelTime.Appearance.Options.UseFont = true;
+			this.labelTime.Appearance.Options.UseTextOptions = true;
+			this.labelTime.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+			this.labelTime.Location = new System.Drawing.Point(2, 90);
+			this.labelTime.Name = "labelTime";
+			this.labelTime.Size = new System.Drawing.Size(644, 45);
+			this.labelTime.StyleController = this.layoutControl2;
+			this.labelTime.TabIndex = 7;
+			this.labelTime.Text = "labelControl2";
+			// 
+			// layoutControlItem5
+			// 
+			this.layoutControlItem5.Control = this.labelTime;
+			this.layoutControlItem5.Location = new System.Drawing.Point(0, 88);
+			this.layoutControlItem5.MaxSize = new System.Drawing.Size(0, 49);
+			this.layoutControlItem5.MinSize = new System.Drawing.Size(107, 49);
+			this.layoutControlItem5.Name = "layoutControlItem5";
+			this.layoutControlItem5.Size = new System.Drawing.Size(648, 49);
+			this.layoutControlItem5.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+			this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
+			this.layoutControlItem5.TextVisible = false;
+			// 
+			// emptySpaceItem5
+			// 
+			this.emptySpaceItem5.AllowHotTrack = false;
+			this.emptySpaceItem5.Location = new System.Drawing.Point(0, 137);
+			this.emptySpaceItem5.Name = "emptySpaceItem5";
+			this.emptySpaceItem5.Size = new System.Drawing.Size(648, 69);
+			this.emptySpaceItem5.TextSize = new System.Drawing.Size(0, 0);
+			// 
+			// btnReset
+			// 
+			this.btnReset.Location = new System.Drawing.Point(2, 244);
+			this.btnReset.Name = "btnReset";
+			this.btnReset.Size = new System.Drawing.Size(644, 32);
+			this.btnReset.StyleController = this.layoutControl2;
+			this.btnReset.TabIndex = 8;
+			this.btnReset.Text = "重置";
+			this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+			// 
+			// layoutControlItem6
+			// 
+			this.layoutControlItem6.Control = this.btnReset;
+			this.layoutControlItem6.Location = new System.Drawing.Point(0, 242);
+			this.layoutControlItem6.Name = "layoutControlItem6";
+			this.layoutControlItem6.Size = new System.Drawing.Size(648, 36);
+			this.layoutControlItem6.TextSize = new System.Drawing.Size(0, 0);
+			this.layoutControlItem6.TextVisible = false;
 			// 
 			// Mainform
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1219, 652);
+			this.ClientSize = new System.Drawing.Size(650, 521);
+			this.Controls.Add(this.layoutControl1);
+			this.KeyPreview = true;
 			this.Name = "Mainform";
 			this.Text = "Mainform";
+			this.Load += new System.EventHandler(this.Mainform_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Mainform_KeyDown);
+			((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+			this.layoutControl1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
+			this.xtraTabControl1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+			this.xtraTabPage1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.layoutControl2)).EndInit();
+			this.layoutControl2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private DevExpress.XtraLayout.LayoutControl layoutControl1;
+		private DevExpress.XtraLayout.LayoutControlGroup Root;
+		private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
+		private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
+		private DevExpress.XtraLayout.LayoutControl layoutControl2;
+		private DevExpress.XtraEditors.SimpleButton btnOption;
+		private DevExpress.XtraEditors.LabelControl labelControl1;
+		private System.Windows.Forms.TextBox tbMinutes;
+		private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+		private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+		private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+		private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+		private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+		private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
+		private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+		private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
+		private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem3;
+		private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem4;
+		private System.Windows.Forms.Timer timer1;
+		private DevExpress.XtraEditors.LabelControl labelTime;
+		private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+		private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem5;
+		private DevExpress.XtraEditors.SimpleButton btnReset;
+		private DevExpress.XtraLayout.LayoutControlItem layoutControlItem6;
 	}
 }
--- Tools/Mainform.cs
+++ Tools/Mainform.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using DevExpress.Services;
 
 namespace Tools
 {
@@ -17,5 +18,130 @@ namespace Tools
 		{
 			InitializeComponent();
 		}
+		#region 事件方法
+		private void Mainform_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Space || e.KeyCode == Keys.Enter)
+			{
+				this.btnOption.PerformClick();
+			}
+		}
+		private void btnOption_Click(object sender, EventArgs e)
+		{
+			if (this.btnOption.Text == "暂停")
+			{
+				this.timer1.Stop();
+				this.tbMinutes.Enabled = true;
+				this.btnOption.Text = "开始";
+				return;
+			}
+			if (this.labelTime.Text == "倒计时剩余时间: ")
+			{
+				GetTime();
+				TimeShow();
+			}
+			this.timer1.Start();
+			this.tbMinutes.Enabled = false;
+			this.btnOption.Text = "暂停";
+		}
+
+		private void Mainform_Load(object sender, EventArgs e)
+		{
+			//this.labelTime.Text = "倒计时剩余时间: ";
+			GetTime();
+			TimeShow();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			TimeShow();
+			this.m_Time -= 1;
+			if (this.m_Time < 0)
+			{
+				this.timer1.Stop();
+				this.tbMinutes.Enabled = true;
+				this.btnOption.Text = "开始";
+			}
+		}
+		private void btnReset_Click(object sender, EventArgs e)
+		{
+			if (this.timer1.Enabled)
+			{
+				XtraMessageBox.Show("倒计时进行中, 请先暂定当前倒计时! ", "小工具", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+			GetTime();
+			TimeShow();
+		}
+		private void tbMinutes_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (this.timer1.Enabled)
+			{
+				XtraMessageBox.Show("倒计时进行中, 请先暂定当前倒计时! ", "小工具", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+		}
+		private void tbMinutes_TextChanged(object sender, EventArgs e)
+		{
+			if (this.timer1.Enabled)
+			{
+				XtraMessageBox.Show("倒计时进行中, 请先暂定当前倒计时! ", "小工具", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+			GetTime();
+			TimeShow();
+		}
+		#endregion
+
+		#region 私有变量
+		/// <summary>
+		/// 存放倒计时时间
+		/// </summary>
+		private double m_Time = 0.00;
+		#endregion
+
+		#region 私有方法
+		/// <summary>
+		/// 倒计时
+		/// </summary>
+		/// <param name="time">秒数</param>
+		private void TimeShow()
+		{
+			if (this.m_Time <= 30)
+			{
+				this.labelTime.ForeColor = Color.Red;
+			}
+			double minutes = Math.Floor(this.m_Time / 60);
+			double seconds = this.m_Time % 60;
+			this.labelTime.Text = $"倒计时剩余时间: {minutes}分{seconds}秒";
+		}
+		/// <summary>
+		/// 获取倒计时时间
+		/// </summary>
+		private void GetTime()
+		{
+			if (this.tbMinutes.Text.Length <= 0)
+			{
+				XtraMessageBox.Show("请输入倒计时时间", "小工具", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+			else
+			{
+				double time = 0;
+				if (!double.TryParse(this.tbMinutes.Text, out time))
+				{
+					XtraMessageBox.Show("请输入正确的时间", "小工具", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				}
+				else
+				{
+					this.timer1.Interval = 1000;
+					this.m_Time = time * 60;
+				}
+			}
+		}
+
+
+		#endregion
+
+
 	}
 }
